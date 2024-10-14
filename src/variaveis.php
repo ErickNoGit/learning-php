1dfce79902fcf813e8aac7a83a79ab4141c3d77c
ErickNoGit <onemylinux@gmail.com>
2024-10-14 18:32:31 -0300

    Variaveis primitivas do PHP

---
+++ src/variaveis.php
@@ -0,0 +1,7 @@
+<?php
+    $inteiro = 42;         // Tipo int
+    $flutuante = 3.1416;   // Tipo float
+    $texto = "Olá PHP";    // Tipo string
+    $verdadeiro = true;    // Tipo bool
+    $inexistente = null;   // Tipo null
+?>
